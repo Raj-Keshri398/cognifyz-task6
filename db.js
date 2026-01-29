43909285b1bf33159209def6783529c2c7b4859e
Raj-Keshri398 <keshriraj093@gmail.com>
2026-01-29 22:08:40 +0530

    Enable SSL for Railway MySQL

--- db.js
+++ db.js
@@ -1,11 +1,14 @@
 import mysql from "mysql2";
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST || "localhost",
-    user: process.env.DB_USER || "root",
-    password: process.env.DB_PASS || "",
-    database: process.env.DB_NAME || "cognifyz_db",
-    port: process.env.DB_PORT || 3306
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 db.connect(err => {
